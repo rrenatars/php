68abc828fed46065b4bfdea9eabcd6f7b2f2d9c7
rrenatars <renat.arslanov2015@yandex.ru>
2023-04-24 13:44:52 +0300

    исправил 4_2

--- lws/lw4/task2/user.php
+++ lws/lw4/task2/user.php
@@ -1,18 +1,17 @@
 <?php
 require_once "../../../lib/database.php";
 
+const firstName = 'first_name';
+const lastName = 'last_name';
+const middleName = 'middle_name';
+const gender = 'gender';
+const birthDate = 'birth_date';
+const email = 'email';
+const phone = 'phone';
+const avatarPath = 'avatar_path';
+
 function saveUserToDatabase(PDO $connection, array $userParams): int
 {
-    // Добавляет пользователя в таблицу 'user' с помощью INSERT.
-    // Возвращает целочисленный ID добавленного пользователя.
-    $firstName = $connection->quote($userParams['first_name']);
-    $lastName = $connection->quote($userParams['last_name']);
-    $middleName = $connection->quote($userParams['middle_name']);
-    $gender = $connection->quote($userParams['gender']);
-    $birthDate = $connection->quote($userParams['birth_date']);
-    $email = $connection->quote($userParams['email']);
-    $phone = $connection->quote($userParams['phone']);
-    $avatarPath = $connection->quote($userParams['avatar_path']);
     $query = <<<SQL
         INSERT INTO 
             user (
@@ -25,32 +24,34 @@ function saveUserToDatabase(PDO $connection, array $userParams): int
                 phone, 
                 avatar_path
             ) 
-        VALUES (
-            $firstName, 
-            $lastName, 
-            $middleName, 
-            $gender, 
-            $birthDate, 
-            $email,
-            $phone,
-            $avatarPath
-        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     SQL;
-    $connection -> exec($query);
-    return (int)$connection -> lastInsertId();
+    $statement = $connection->prepare($query);
+    $statement->execute([
+        $userParams[firstName],
+        $userParams[lastName],
+        $userParams[middleName],
+        $userParams[gender],
+        $userParams[birthDate],
+        $userParams[email],
+        $userParams[phone],
+        $userParams[avatarPath]
+    ]);
+    return (int)$connection->lastInsertId();
 }
 
 // Проверка данных формы
-if (isset($_POST['last_name']) && isset($_POST['first_name']) && isset($_POST['gender']) && isset($_POST['birth_date']) && isset($_POST ['email'])) {
+if (isset($_POST[lastName]) && isset($_POST[firstName]) && isset($_POST[gender]) && isset($_POST[birthDate]) && isset($_POST [email]))
+{
     $userParams = [
-        'last_name' => $_POST['last_name'],
-        'first_name' => $_POST['first_name'],
-        'middle_name' => $_POST['middle_name'],
-        'gender' => $_POST['gender'],
-        'birth_date' => $_POST['birth_date'],
-        'email' => $_POST['email'],
-        'phone' => $_POST['phone'],
-        'avatar_path' => $_POST['avatar_path']
+        'last_name' => $_POST[lastName],
+        'first_name' => $_POST[firstName],
+        'middle_name' => $_POST[middleName],
+        'gender' => $_POST[gender],
+        'birth_date' => $_POST[birthDate],
+        'email' => $_POST[email],
+        'phone' => $_POST[phone],
+        'avatar_path' => $_POST[avatarPath]
     ];
     $connection = connectDatabase();
     saveUserToDatabase($connection, $userParams);
